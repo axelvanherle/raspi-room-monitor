e778e5e3227b5979c7c4eb543ad396e678b9dafc
xel <axelvanherle@outlook.com>
2024-02-07 15:54:14 +0100

    Test

--- src/datacollector.py
+++ src/datacollector.py
@@ -1,7 +1,6 @@
 import psutil
 from prometheus_client import start_http_server, Gauge
 import time
-# test
 
 cpu_usage = Gauge('cpu_usage', 'CPU Usage', ['core'])
 ram_usage = Gauge('ram_usage', 'RAM Usage')
