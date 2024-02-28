37004d40b8e00caa1b50d561120d70b9550fa080
xel <axelvanherle@outlook.com>
2024-02-28 21:47:15 +0100

    fix bug

--- src/bh1750_metrics.py
+++ src/bh1750_metrics.py
@@ -7,5 +7,6 @@ bus = smbus.SMBus(1)
 light_intensity_gauge = Gauge('room_light_intensity_lux', 'Room Light Intensity in Lux')
 
 def collect_bh1750_metrics():
-    light_level = bus.read_i2c_block_data(bh1750_addr, 0x11)
+    data = bus.read_i2c_block_data(bh1750_addr, 0x11)
+    light_level = (data[1] + (256 * data[0])) / 1.2
     light_intensity_gauge.set(light_level)
